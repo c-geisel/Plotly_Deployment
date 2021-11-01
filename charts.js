ee5dccd5b41d52daa5e2f58e752e6a8584e80d3a
Chloe Ittel <chloeittel@Chloes-Air.woh.rr.com>
2021-11-01 08:41:01 -0400

    updates

--- charts.js
+++ charts.js
@@ -90,5 +90,27 @@ function buildCharts(sample) {
     };
     // 10. Use Plotly to plot the data with the layout. 
     Plotly.newPlot("bar", barData, barLayout);
+
+    // 1. Create the trace for the bubble chart.
+    var bubbleData = [{
+      x: otu_ids,
+      y: sample_values,
+      text: otu_labels,
+      mode: 'markers',
+      marker: {
+        color: otu_ids,
+        size: sample_values,
+      }
+   
+    }];
+
+    // 2. Create the layout for the bubble chart.
+    var bubbleLayout = {
+      
+    };
+
+    // 3. Use Plotly to plot the data with the layout.
+    Plotly.newPlot("bubble", bubbleData); 
+    
   });
 }
